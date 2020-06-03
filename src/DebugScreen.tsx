22c6ffd9ca118930f37cfb3a8889e949b82a7f82
Evan Rosson <git@erosson.org>
2020-06-02 22:22:47 -0400

    debug scrolls, dumps asyncstorage

--- src/DebugScreen.tsx
+++ src/DebugScreen.tsx
@@ -1,43 +1,60 @@
 import React from "react";
-import { View, Text, Switch, Platform } from "react-native";
+import { ScrollView, View, Text, Switch, Platform, AsyncStorage } from "react-native";
 import Constants from "expo-constants";
 import * as Feature from "./feature";
 import version from "../.version.json"
 
 export default () => {
-    const {feature, updateFeature} = React.useContext(Feature.Context);
-    const items = [
-      ["Release channel", Constants.manifest.releaseChannel || "(dev)"],
-      ["Installation id", Constants.installationId],
-      ["Expo version", Constants.expoVersion],
-      ["App version", Constants.manifest.version],
-      ["Revision", Constants.manifest.revisionId || "(dev)"],
-      ["Revision Git", version.hash],
-      ["Revision Date", version.date],
-      ["Revision Timestamp", version.timestamp + ''],
-      ["OS", Platform.OS],
-      ...(Object.entries(feature)
-        // @ts-ignore: sort features by name. I promise key in [key, value] is a string
-        .sort((a, b) => a[0] > b[0])
-        .map(([key, val]: [string, boolean]) => (
-          [key,
-          <Switch
-            value={val}
-            // @ts-ignore
-            onChange={() => updateFeature({[key]: !val})}
-          />
-          ]
-        )
-      ))
-    ]
-    return (
+  const {feature, updateFeature} = React.useContext(Feature.Context);
+  const [async_, setAsync] = React.useState(null);
+  const [dump, setDump] = React.useState(false);
+  React.useEffect(() => {
+    if (dump && async_ === null) {
+      (async () => {
+        const keys = await AsyncStorage.getAllKeys();
+        const dump = await AsyncStorage.multiGet(keys);
+        setAsync(dump);
+      })();
+    }
+  });
+  const items = [
+    ["Release channel", Constants.manifest.releaseChannel || "(dev)"],
+    ["Installation id", Constants.installationId],
+    ["Expo version", Constants.expoVersion],
+    ["App version", Constants.manifest.version],
+    ["Revision", Constants.manifest.revisionId || "(dev)"],
+    ["Revision Git", version.hash],
+    ["Revision Date", version.date],
+    ["Revision Timestamp", version.timestamp + ''],
+    ["OS", Platform.OS],
+    ...(Object.entries(feature)
+      // @ts-ignore: sort features by name. I promise key in [key, value] is a string
+      .sort((a, b) => a[0] > b[0])
+      .map(([key, val]: [string, boolean]) => (
+        [key,
+        <Switch
+          value={val}
+          onValueChange={() => updateFeature({[key]: !val})}
+        />
+        ]
+      )
+    )),
+    ["Dump AsyncStorage?", <Switch value={dump} onValueChange={() => setDump(!dump)} />],
+    ...(dump ? (async_ || []) : [])
+      .map(([key, val]: [string, string]) => (
+        ["AsyncStorage[\""+key+"\"]: \n"+val,
+        ]
+      )
+    ),
+  ]
+  return (
+    <ScrollView>
+      <Text style={{fontSize: 24, borderBottomWidth: 1}}>Debug</Text>
       <View>
-        <Text style={{fontSize: 24, borderBottomWidth: 1}}>Debug</Text>
-        <View>
-          {items.map(renderEntry)}
-        </View>
+        {items.map(renderEntry)}
       </View>
-    )
+    </ScrollView>
+  )
 };
 
 function renderEntry([key, val], i) {
