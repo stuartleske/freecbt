de59d97c24a1dc3e8eedd82d9535d4d7f3847cef
Evan Rosson <git@erosson.org>
2020-06-07 17:07:09 -0400

    dont log to sentry in dev

--- src/sentry/index.ts
+++ src/sentry/index.ts
@@ -3,7 +3,7 @@ import * as Sentry from "sentry-expo";
 Sentry.init({
   dsn:
     "https://2a90a942f052449db134f1131a7649fd@o392715.ingest.sentry.io/5240752",
-  enableInExpoDevelopment: true,
+  // enableInExpoDevelopment: true,
   // debug: true,
 });
 
