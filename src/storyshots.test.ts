767072e38ac0f290a71d068a986ea3db54018e7b
Evan Rosson <git@erosson.org>
2020-06-07 16:31:32 -0400

    ugh, formatting again. #27

--- src/storyshots.test.ts
+++ src/storyshots.test.ts
@@ -11,8 +11,10 @@ jest.mock("react-native/Libraries/Animated/src/NativeAnimatedHelper");
 // * Quiet, AsyncStorage. I'd love to upgrade you, but can't yet - we're locked
 // to the version used by Expo.
 const consoleError = console.error;
-jest.spyOn(console, 'error').mockImplementation(msg => {
-  if (msg !== "Warning: AsyncStorage has been extracted from react-native core and will be removed in a future release. It can now be installed and imported from '@react-native-community/async-storage' instead of 'react-native'. See https://github.com/react-native-community/react-native-async-storage") {
+const asyncStorageMsg =
+  "Warning: AsyncStorage has been extracted from react-native core and will be removed in a future release. It can now be installed and imported from '@react-native-community/async-storage' instead of 'react-native'. See https://github.com/react-native-community/react-native-async-storage";
+jest.spyOn(console, "error").mockImplementation((msg) => {
+  if (msg !== asyncStorageMsg) {
     consoleError.call(console, msg);
   }
 });
