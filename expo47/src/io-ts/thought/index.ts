08c5e8b0ed253f52b4f727887d615031463dabae
Evan Rosson <git@erosson.org>
2022-12-07 20:18:54 +0000

    io-ts thoughtstore

--- expo47/src/io-ts/thought/index.ts
+++ expo47/src/io-ts/thought/index.ts
@@ -1,3 +1,4 @@
 export { Thought, ID, create, Group, groupByDay } from "./thought"
 export { Persist, Legacy, VERSION } from "./persist"
 export { Codec, FromLegacy } from "./codec"
+export * as Store from './store'
---
+++ expo47/src/io-ts/thought/store.ts
@@ -0,0 +1,96 @@
+import { AsyncStorage } from "react-native"
+import * as AsyncState from "../../async-state"
+import { Thought, ID, THOUGHTS_KEY_PREFIX } from "./thought"
+import { Persist } from "./persist"
+import { Codec } from "./codec"
+import { decodeOrThrow } from "../io-utils"
+
+const EXISTING_USER_KEY = "@Quirk:existing-user"
+
+export async function exists(key: ID): Promise<boolean> {
+  try {
+    const value = await AsyncStorage.getItem(key)
+    return !!value
+  } catch (err) {
+    console.error(err)
+    return false
+  }
+}
+
+export async function getIsExistingUser(): Promise<boolean> {
+  try {
+    const value = await AsyncStorage.getItem(EXISTING_USER_KEY)
+    return !!value
+  } catch (err) {
+    console.error(err)
+    return false
+  }
+}
+
+export async function setIsExistingUser() {
+  try {
+    await AsyncStorage.setItem(EXISTING_USER_KEY, "true")
+  } catch (err) {
+    console.error(err)
+  }
+}
+
+export async function write(t: Thought): Promise<void> {
+  const enc = Codec.encode(t)
+  const raw = JSON.stringify(enc)
+  await AsyncStorage.setItem(t.uuid, raw)
+}
+
+export async function read(id: ID): Promise<Thought> {
+  return parseOrThrow((await AsyncStorage.getItem(id)) ?? "")
+}
+export async function readResult(
+  id: ID
+): Promise<AsyncState.Result<Thought, ParseError>> {
+  return parseResult((await AsyncStorage.getItem(id)) ?? "", id)
+}
+
+export interface ParseError {
+  error: any
+  raw: string
+  id: ID
+}
+function parseOrThrow(raw: string): Thought {
+  const enc = JSON.parse(raw)
+  return decodeOrThrow(Codec, enc)
+}
+function parseResult(
+  raw: string,
+  id: ID
+): AsyncState.Result<Thought, ParseError> {
+  return AsyncState.mapError(
+    AsyncState.tryResult(() => parseOrThrow(raw)),
+    (error) => ({ error, raw, id })
+  )
+}
+
+export async function remove(uuid: ID) {
+  try {
+    await AsyncStorage.removeItem(uuid)
+  } catch (error) {
+    console.error(error)
+  }
+}
+
+export async function getExercises(): Promise<
+  AsyncState.Result<Thought, ParseError>[]
+> {
+  const keys = (await AsyncStorage.getAllKeys()).filter((key) =>
+    key.startsWith(THOUGHTS_KEY_PREFIX)
+  )
+
+  let rows = await AsyncStorage.multiGet(keys)
+  return rows.map(([key, raw]: [string, string | null]) =>
+    parseResult(raw ?? "", key)
+  )
+}
+
+export const countThoughts = async (): Promise<number> => {
+  const exercises = await getExercises()
+  return exercises.length
+}
